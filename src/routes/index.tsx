b2f63b7cc322feee0d7b535fbbcace48203325ec
paulbgtr <pbg.work@pm.me>
2026-02-21 15:47:20 +0200

    improve index route layout

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,22 +2,26 @@ import { AudioPlayer } from "~/components/audio-player";
 
 export default function Home() {
   return (
-    <main>
-      <div>
-        Forest: <AudioPlayer soundPath="/audio/forest.mp3" />
-      </div>
-      <div>
-        Fireplace: <AudioPlayer soundPath="/audio/fireplace.mp3" />
-      </div>
-      <div>
-        Cafe: <AudioPlayer soundPath="/audio/cafe.mp3" />
-      </div>
-      <div>
-        Ocean: <AudioPlayer soundPath="/audio/ocean.mp3" />
-      </div>
-      <div>
-        Rain: <AudioPlayer soundPath="/audio/rain.mp3" />
-      </div>
+    <main class="h-screen">
+      <section class="flex items-center justify-center h-full">
+        <div>
+          <div>
+            Forest: <AudioPlayer soundPath="/audio/forest.mp3" />
+          </div>
+          <div>
+            Fireplace: <AudioPlayer soundPath="/audio/fireplace.mp3" />
+          </div>
+          <div>
+            Cafe: <AudioPlayer soundPath="/audio/cafe.mp3" />
+          </div>
+          <div>
+            Ocean: <AudioPlayer soundPath="/audio/ocean.mp3" />
+          </div>
+          <div>
+            Rain: <AudioPlayer soundPath="/audio/rain.mp3" />
+          </div>
+        </div>
+      </section>
     </main>
   );
 }
